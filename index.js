e1c2acd9887405d31477062d8e70a0dd788f961d
Dario Di Gulio <DGDarioGabriel@proveedor.teco.com.ar>
2026-02-12 11:51:13 -0300

    Dinamic port

--- index.js
+++ index.js
@@ -78,4 +78,6 @@ app.post("/slack/events", (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("Listening on :3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on :${port}`));
+
